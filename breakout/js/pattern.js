dba26027a5dd6d0699cf735a9bf025a41a405a19
Aditya Parmar <parmar415@gmail.com>
2017-10-26 21:35:37 -0700

    Add 2 pattern for stage 1

---
+++ breakout/js/pattern.js
@@ -0,0 +1,100 @@
+var images = ['redapple','greenapple','custardapple','orange','peru','watermelon','banana','blackberry']
+var image = Math.floor(Math.random() * (7 - 0)) + 0;
+function pattern0(bricks){
+   
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+    {
+        for (var x = 0; x < 13; x++)
+        {
+            inside_brick = bricks.create(100 + (x * 46), 100 + (y * 62), images[image]);
+            inside_brick.scale.setTo(.08, .08);
+            inside_brick.body.bounce.set(1);
+            inside_brick.body.immovable = true;
+        }
+    }
+    return inside_brick;    
+}
+function pattern1(bricks){
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+        {
+            if(y%2==0){
+                for (var x = 0; x < 13; x++)
+                {
+                    if(x%2==0){
+                        inside_brick = bricks.create(100 + (x * 46), 100 + (y * 62), images[image]);
+                        inside_brick.scale.setTo(.08, .08);
+                        inside_brick.body.bounce.set(1);
+                        inside_brick.body.immovable = true;
+                    }
+                   
+                }
+            }
+            else{
+                for (var x = 0; x < 13; x++)
+                {
+                    if(x%2!=0){
+                        inside_brick = bricks.create(100 + (x * 46), 100 + (y * 62), images[image]);
+                        inside_brick.scale.setTo(.08, .08);
+                        inside_brick.body.bounce.set(1);
+                        inside_brick.body.immovable = true;
+                    }
+                }
+            }
+           
+        }
+    return inside_brick;    
+}
+function pattern2(bricks){
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                inside_brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
+                inside_brick.body.bounce.set(1);
+                inside_brick.body.immovable = true;
+            }
+        }
+    return inside_brick;    
+}
+function pattern3(bricks){
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                inside_brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
+                inside_brick.body.bounce.set(1);
+                inside_brick.body.immovable = true;
+            }
+        }
+    return inside_brick;    
+}
+function pattern4(bricks){
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                inside_brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
+                inside_brick.body.bounce.set(1);
+                inside_brick.body.immovable = true;
+            }
+        }
+    return inside_brick;    
+}
+function pattern5(bricks){
+    var inside_brick;
+    for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                inside_brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
+                inside_brick.body.bounce.set(1);
+                inside_brick.body.immovable = true;
+            }
+        }
+    return inside_brick;    
+}
