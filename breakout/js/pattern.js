2129a9a2c45b4a94a8696fc82299c79fa4b690bc
Aditya Parmar <parmar415@gmail.com>
2017-10-27 18:19:35 -0700

    4 pattern implemented.

--- breakout/js/pattern.js
+++ breakout/js/pattern.js
@@ -6,11 +6,11 @@ var image2 = Math.floor(Math.random() * (7 - 0)) + 0;
 function pattern0(bricks){
    
     var inside_brick;
-    for (var y = 0; y < 4; y++)
+    for (var y = 0; y < 5; y++)
     {
-        for (var x = 0; x < 12; x++)
+        for (var x = 0; x < 15; x++)
         {
-            inside_brick = bricks.create(110 + (x * 46), 100 + (y * 62), images[image]);
+            inside_brick = bricks.create(100 + (x * 56), 80 + (y * 72), images[image]);
             inside_brick.scale.setTo(.08, .08);
             inside_brick.body.bounce.set(1);
             inside_brick.body.immovable = true;
@@ -20,13 +20,13 @@ function pattern0(bricks){
 }
 function pattern1(bricks){
     var inside_brick;
-    for (var y = 0; y < 4; y++)
+    for (var y = 0; y < 5; y++)
         {
             if(y%2==0){
-                for (var x = 0; x < 12; x++)
+                for (var x = 0; x < 15; x++)
                 {
                     if(x%2==0){
-                        inside_brick = bricks.create(110 + (x * 46), 100 + (y * 62), images[image]);
+                        inside_brick = bricks.create(100 + (x * 56), 80 + (y * 72), images[image]);
                         inside_brick.scale.setTo(.08, .08);
                         inside_brick.body.bounce.set(1);
                         inside_brick.body.immovable = true;
@@ -35,10 +35,10 @@ function pattern1(bricks){
                 }
             }
             else{
-                for (var x = 0; x < 12; x++)
+                for (var x = 0; x < 15; x++)
                 {
                     if(x%2!=0){
-                        inside_brick = bricks.create(110 + (x * 46), 100 + (y * 62), images[image]);
+                        inside_brick = bricks.create(100 + (x * 56), 80 + (y * 72), images[image]);
                         inside_brick.scale.setTo(.08, .08);
                         inside_brick.body.bounce.set(1);
                         inside_brick.body.immovable = true;
@@ -74,6 +74,31 @@ function pattern2(bricks){
                             inside_brick.body.immovable = true; 
                          }
                      }
+               
+            
+           
+        }
+    }
+    return inside_brick;    
+}
+function pattern3(bricks){
+    var inside_brick;
+    for (var y = 0; y < 5; y++)
+    {
+        for (var x = 0; x < 15; x++)
+        {
+                if( (y==0&(x==0||x==4||x==5||x==9||x==10 ||x==14))||
+                    (y==1&( x!=2 & x!=7 & x!=12)) ||
+                    (y==2) ||
+                    (y==3&(x!=2 & x!=7 & x!=12)) ||
+                    (y==4&(x==0||x==4 ||x==5||x==9||x==10||x==14))
+                     ){
+                        inside_brick = bricks.create(100 + (x * 56), 80 + (y * 72), images[image]);
+                        inside_brick.scale.setTo(.08, .08);
+                        inside_brick.body.bounce.set(1);
+                        inside_brick.body.immovable = true;
+                     }
+                    
                
             
            
